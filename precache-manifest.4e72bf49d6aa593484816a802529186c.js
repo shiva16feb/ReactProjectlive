32e29b73a634866917fd9b888e37e0648a5e9c22
shiva16feb <shivanandverma69@gmail.com>
2022-01-23 12:13:01 +0530

    Updates

---
+++ precache-manifest.4e72bf49d6aa593484816a802529186c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "951448e4593e5239a1bd3cd1e278a1b9",
+    "url": "/ReactProjectlive/index.html"
+  },
+  {
+    "revision": "431e9584cc7d46e1651e",
+    "url": "/ReactProjectlive/static/css/2.68a81a89.chunk.css"
+  },
+  {
+    "revision": "e9bc957f8c9c56085ea3",
+    "url": "/ReactProjectlive/static/css/main.981eb92e.chunk.css"
+  },
+  {
+    "revision": "431e9584cc7d46e1651e",
+    "url": "/ReactProjectlive/static/js/2.a5eb0679.chunk.js"
+  },
+  {
+    "revision": "e9bc957f8c9c56085ea3",
+    "url": "/ReactProjectlive/static/js/main.779e0b23.chunk.js"
+  },
+  {
+    "revision": "a6ce5a5d9855b8d00d0b",
+    "url": "/ReactProjectlive/static/js/runtime~main.4499d4b4.js"
+  },
+  {
+    "revision": "7c032f8d0f0e68356d123826d06f7009",
+    "url": "/ReactProjectlive/static/media/apng.7c032f8d.png"
+  },
+  {
+    "revision": "e858a14b5a66b7ca7036f96b4f0f512c",
+    "url": "/ReactProjectlive/static/media/hjpeg.e858a14b.jpeg"
+  },
+  {
+    "revision": "b4cd8e57d85271f9362913028dae148b",
+    "url": "/ReactProjectlive/static/media/robot.b4cd8e57.png"
+  },
+  {
+    "revision": "0d52cc9ba43d02008f244a73266c0469",
+    "url": "/ReactProjectlive/static/media/sjpeg.0d52cc9b.jpeg"
+  }
+]);
